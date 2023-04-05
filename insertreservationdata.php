b4d9b2d9f76a77db463a27f716c686115d6703f7
OfficieelJoran <91888385+OfficieelJoran@users.noreply.github.com>
2023-04-05 09:15:28 +0200

    Add files via upload

--- insertreservationdata.php
+++ insertreservationdata.php
@@ -2,6 +2,8 @@
 include 'db.php';
 
 
+
+
 $result = mysqli_query($conn, $sql);
 
 // while ($row = mysqli_fetch_assoc($result)) {
@@ -19,3 +21,5 @@ $occupied = $_POST['occupied'];
 
 mysqli_query($conn, "INSERT INTO reserveren (stoelnummerid, datum, begintijd, eindtijd, phonenumber, occupied)
  VALUES ('$stoelnummerid', '$datum', '$begintijd', '$eindtijd', '$phonenumber', '$occupied')");
+
+ 
